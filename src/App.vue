5677bce2fd6b897f38eebdb6a896d5b4ee3eb7e2
Moaz Ayman <ma5746@fayoum.edu.eg>
2024-04-21 01:50:38 +0200

    Inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template>  
+  <h1>Reaction Timer</h1>
+  <p>Click the button when the block appears as fast as you can</p>
+  <div class="buttons">
+    <button @click="start" :disabled="isPlaying"><span></span><p data-start="good luck!" data-text="start!" data-title="new game"></p></button>
+  </div>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="score && !isPlaying" :score="score"/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Block,Results
+  },
+   data(){
+    return{
+      isPlaying: false,
+      delay: null,
+      score: null
+    }
+  },
+  methods:{
+    start(){
+      this.delay = Math.random() * 5000 + 2000
+      this.isPlaying = true
+    },
+    endGame(reactionTime){
+      this.isPlaying = false
+      this.score = reactionTime
+    }
+  },
 }
 </script>
 
@@ -20,7 +43,188 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
+.buttons {
+  display: flex;
+  justify-content: space-around;
+  top: 20px;
+  left: 20px;
+}
+.buttons button:disabled {
+  cursor: not-allowed;
+  opacity: 0.5;
+}
+.buttons button:disabled ::before, .buttons button:disabled ::after{
+  content: attr(data-start);  
+}
+
+.buttons button {
+  width: 150px;
+  height: 50px;
+  background-color: white;
+  margin: 20px;
+  color: #568fa6;
+  position: relative;
+  overflow: hidden;
+  font-size: 14px;
+  letter-spacing: 1px;
+  font-weight: 500;
+  text-transform: uppercase;
+  transition: all 0.3s ease;
+  cursor: pointer;
+  border: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 3px;
+}
+
+.buttons button:before, .buttons button:after {
+  content: "";
+  position: absolute;
+  width: 0;
+  height: 2px;
+  background-color: #44d8a4;
+  transition: all 0.3s cubic-bezier(0.35, 0.1, 0.25, 1);
+}
+
+.buttons button:before {
+  right: 0;
+  top: 0;
+  transition: all 0.5s cubic-bezier(0.35, 0.1, 0.25, 1);
+}
+
+.buttons button:after {
+  left: 0;
+  bottom: 0;
+}
+
+.buttons button span {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  margin: 0;
+  padding: 0;
+  z-index: 1;
+}
+
+.buttons button span:before, .buttons button span:after {
+  content: "";
+  position: absolute;
+  width: 2px;
+  height: 0;
+  background-color: #44d8a4;
+  transition: all 0.3s cubic-bezier(0.35, 0.1, 0.25, 1);
+}
+
+.buttons button span:before {
+  right: 0;
+  top: 0;
+  transition: all 0.5s cubic-bezier(0.35, 0.1, 0.25, 1);
+}
+
+.buttons button span:after {
+  left: 0;
+  bottom: 0;
+}
+
+.buttons button p {
+  padding: 0;
+  margin: 0;
+  transition: all 0.4s cubic-bezier(0.35, 0.1, 0.25, 1);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.buttons button p:before, .buttons button p:after {
+  position: absolute;
+  width: 100%;
+  transition: all 0.4s cubic-bezier(0.35, 0.1, 0.25, 1);
+  z-index: 1;
+  left: 0;
+}
+
+.buttons button p:before {
+  content: attr(data-title);
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.buttons button p:after{
+  content: attr(data-text);
+  top: 150%;
+  color: #44d8a4;
+}
+
+.buttons button:hover:before, .buttons button:hover:after {
+  width: 100%;
+}
+
+.buttons button:hover span {
+  z-index: 1;
+}
+
+.buttons button:hover span:before, .buttons button:hover span:after {
+  height: 100%;
+}
+
+.buttons button:hover p:before {
+  top: -50%;
+  transform: rotate(5deg);
+}
+
+.buttons button:hover p:after {
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.buttons button.start {
+  background-color: #44d8a4;
+  box-shadow: 0px 5px 10px -10px rgba(0, 0, 0, 0.2);
+  transition: all 0.2s ease;
+}
+
+.buttons button.start p:before {
+  top: -50%;
+  transform: rotate(5deg);
+}
+
+.buttons button.start p:after {
+  color: white;
+  transition: all 0s ease;
+  content: attr(data-start);
+  top: 50%;
+  transform: translateY(-50%);
+  animation: start 0.3s ease;
+  animation-fill-mode: forwards;
+}
+
+@keyframes start {
+  from {
+    top: -50%;
+  }
+}
+
+.buttons button.start:hover:before, .buttons button.start:hover:after {
+  display: none;
+}
+
+.buttons button.start:hover span {
+  display: none;
+}
+
+.buttons button:active {
+  outline: none;
+  border: none;
+}
+
+.buttons button:focus {
+  outline: 0;
+}
+
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,55 @@
+<template>
+  <h2>Your Reaction Time is {{score}} ms</h2>
+  <p :class="{
+        ninja:this.score<=200,
+        samurai:this.score<=300 && this.score >200,
+        warrior:this.score<=400 && this.score >300,
+        grasshopper:this.score>400
+    }">{{rank}}</p>
+</template>
+
+<script>
+export default {
+    props:['score'],
+    data(){
+        return{
+            rank: ''
+        }
+    },
+    mounted(){
+        if(this.score < 200){
+            this.rank = 'You are a ninja!'
+        }else if(this.score < 300){
+            this.rank = 'You are a samurai!'
+        }else if(this.score < 400){
+            this.rank = 'You are a warrior!'
+        }else{
+            this.rank = 'You are a grasshopper!'
+        }
+    }
+}
+</script>
+
+<style>
+.ninja{
+    color: red;
+    font-size: 1.8rem;
+    font-weight: bold;
+}
+.samurai{
+    color: blue;
+    font-size: 1.8rem;
+    font-weight: bold;
+}
+.warrior{
+    color: green;
+    font-size: 1.8rem;
+    font-weight: bold;
+
+}
+.grasshopper{
+    color: yellow;
+    font-size: 1.8rem;
+    font-weight: bold;
+}
+</style>
